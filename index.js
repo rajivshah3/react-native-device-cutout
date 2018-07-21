1c3e245a7445f31e5d6d54519107c06d2f2e24d8
Rajiv Shah <rajivshah1@icloud.com>
2018-07-21 16:56:27 -0400

    Add JS methods

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 /* eslint-disable import/no-unresolved */
 import { NativeModules } from 'react-native';
 /* eslint-enable import/no-unresolved */
+
+const { RNDeviceCutout } = NativeModules;
+
+export const getBoundingRects = () => RNDeviceCutout.getBoundingRects();
+
+export const getSafeInsetBottom = () => RNDeviceCutout.getSafeInsetBottom();
+
+export const getSafeInsetLeft = () => RNDeviceCutout.getSafeInsetLeft();
+
+export const getSafeInsetRight = () => RNDeviceCutout.getSafeInsetRight();
+
+export const getSafeInsetTop = () => RNDeviceCutout.getSafeInsetTop();
